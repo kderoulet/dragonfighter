97ec4fd3ceee28ca331161e57991ce55fbe6d267
Kevin de Roulet <kderoulet@yahoo.com>
2017-10-12 15:12:43 -0700

    added render functions

--- js/dragonfighter.js
+++ js/dragonfighter.js
@@ -1,32 +1,91 @@
-// (function () {
-//     'use strict';
+$(function() {
     
-var dragonAction = "";
-var humanAction = "";
-var dragonHealth = 100;
-var fireDamage = 40;
-var clawDamage = 10;
-var tailSwipeDamage = 10;
-var flying = false;
-var humanHealth = 100;
-var wandBlastDamage = 7;
+var dragonAction;
+var humanAction;
+var dragonHealth;
+var fireDamage;
+var clawDamage;
+var tailSwipeDamage;
+var flying;
+var humanHealth;
+var wandBlastDamage;
+var page;
+var statusText;
+var descriptionText;
+var actionText;
+var humanMoves = {};
+var dragonMoves = {};
+
+function init() {
+    page = 1;
+    render();
+}
+
+function initVarEasy() {
+    dragonAction = "";
+    humanAction = "";
+    dragonHealth = 100;
+    fireDamage = 60;
+    clawDamage = 15;
+    tailSwipeDamage = 10;
+    flying = false;
+    humanHealth = 100;
+    wandBlastDamage = 7;
+    page = 2;
+}
+function initVarHard() {
+    dragonAction = "";
+    humanAction = "";
+    dragonHealth = 100;
+    fireDamage = 200;
+    clawDamage = 30;
+    tailSwipeDamage = 20;
+    flying = false;
+    humanHealth = 100;
+    wandBlastDamage = 7;
+    page = 2;
+}
 
 
 function nextTurn() {
 if (dragonAction === "inhale") {
     breatheFire();
     dragonAction = "breatheFire";
-} else randomAction(0, );}
+} else randomAction(0, 6);}
 
 function randomAction(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;}
 
-
+//event listener
+$('.input').on('submit', function(evt) {
+    evt.preventDefault();
+    if (page === 1) {
+        console.log($('input').val())    
+        if ($('input').val() == 1) {
+            initVarEasy();
+            page++;        
+        }
+        else if ($('input').val() == 2) {
+            initVarHard();
+            page++;        
+        }
+}
+    if (page === 2) {
+        if ($('input').val() == 1) {
+            $('.description').text('Now it must be working')
+                    
+        }
+        else if ($('input').val() == 2) {
+            initVarHard();
+            $('.description').text('But for real now')        
+        }
+    }
+});
     //Do we want an array of objects rather than a big ol' object?
     
-var dragonMoves = {
+dragonMoves = {
     inhale: function() {
         dragonAction = "inhale";
         console.log("The dragon is strongly inhaling...")
@@ -44,32 +103,69 @@ var dragonMoves = {
     breatheFire: function() {
         dragonAction = "breatheFire";
         if (humanAction === "endureFlame") {
-            console.log("You dodged the dragon's fire!");
+            description2Text = "You dodged the dragon's fire!";
             }
         else if (humanAction === "teleport") {
-            console.log("The dragon breathes fire, but he misses")
+            description2Text = "The dragon breathes fire, but he misses";
         }
         else {
             humanHealth = humanHealth - fireDamage;
-            console.log("The dragon breathes fire, burning you for " + fireDamage + " damage. You have " + humanHealth + " remaining.")}},
+            description2Text = "The dragon breathes fire, burning you for " + fireDamage + " damage. You have " + humanHealth + " remaining."}
+    },
      tailSwipe: function() {
         dragonAction = "tailSwipe";
         humanHealth = humanHealth - tailSwipeDamage;
-         console.log("The dragon sweeps you with his tail, knocking you over and dealing " + tailSwipeDamage + " damage to you. You have " + humanHealth + " remaining.")}
-    };
+        description2Text = "The dragon sweeps you with his tail, knocking you over and dealing " + tailSwipeDamage + " damage to you. You have " + humanHealth + " remaining."
+    }};
 
-var humanMoves = {
+humanMoves = {
     wandBlast: function() {
         humanAction = "wandBlast"
         dragonHealth = dragonHealth - wandBlastDamage;
-        console.log("A blast of magical energy shoots from your wand and hits the dragon! The dragon reacts as if it were stung by a bee.")
+        description1Text = "A blast of magical energy shoots from your wand and hits the dragon! The dragon reacts as if it were stung by a bee."
     },
     endureFlame: function() {
         humanAction = "endureFlame"
-        console.log("You cast endure flame, sheilding you from fire damage for the turn.")
+        description1Text = "You cast endure flame, sheilding you from fire damage for the turn."
     }
-    
+};
 
+function renderPage() {
+    $('.status').text(statusText)
+    $('.description1').text(description1Text)
+    $('.description2').text(description2Text)
+    $('.action').text(actionText)
+    $('input').val('');
 }
 
-// })();
+function setTextValues() {
+    if (page === 1) {
+        statusText = 'Welcome to Game';
+        description1Text = '';
+        description2Text = '';
+        actionText = 'How difficult do you want this? <br> 1. Easy <br> 2. Hard'
+    }
+    else if (page === 2) {
+
+    }
+    else if (page === 3) {
+
+    }
+    else if (page === 4) {
+
+    }
+    else {
+        statusText = 'You Died'
+        descriptionText = '';
+        actionText = 'Do you want to try again? <br> 1. Yes <br> 2. No';
+    }
+}
+
+function render() {
+    setTextValue();
+    renderPage();
+}
+
+init();
+
+})
